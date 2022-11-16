caa081919f769bca1c9015e9cd34b57955824e4e
Chris Millar <cmillar@adobe.com>
2022-11-16 16:50:04 -0700

    build

---
+++ index.js
@@ -0,0 +1,22 @@
+var $E1WXx$reactjsxruntime = require("react/jsx-runtime");
+var $E1WXx$reactdomclient = require("react-dom/client");
+
+
+
+function $524e68bbcd2d60d9$var$App(props) {
+    return /*#__PURE__*/ (0, $E1WXx$reactjsxruntime.jsxs)("h1", {
+        children: [
+            "Hello ",
+            props.name || "world"
+        ]
+    });
+}
+const $524e68bbcd2d60d9$var$block = document.body.querySelector(".hello-world > div > div");
+const $524e68bbcd2d60d9$var$name = $524e68bbcd2d60d9$var$block.textContent;
+const $524e68bbcd2d60d9$var$root = (0, $E1WXx$reactdomclient.createRoot)($524e68bbcd2d60d9$var$block);
+$524e68bbcd2d60d9$var$root.render(/*#__PURE__*/ (0, $E1WXx$reactjsxruntime.jsx)($524e68bbcd2d60d9$var$App, {
+    name: $524e68bbcd2d60d9$var$name
+}));
+
+
+//# sourceMappingURL=index.js.map
